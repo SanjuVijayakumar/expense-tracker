0e0b88b22a232ab6795e3f93958782b0de7475f8
sanju <sanjukalathil05@gmail.com>
2026-09-05 00:25:32 +0530

    project created expense-tracker

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [transations, setTransations] = useState([]);
+
+  return (
+    <>
+      hELLLO WORLD
+    </>
+  )
+}
+
+export default App
